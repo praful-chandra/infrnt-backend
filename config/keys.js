b287ed4682e60c59e31cd5b532a641160deae11b
praful-chandra <44903575+praful-chandra@users.noreply.github.com>
2019-03-04 19:09:41 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,11 @@
 
+module.exports ={
+    googleClientID  : "client id",
+    googleClientSecret : "client secret",
+    mongoURI : "mongo uri",
+    secretOrKey  : "secret key",
+    cookieKey : "cookie key",
+    FACEBOOK_APP_ID : "fbId",
+    FACEBOOK_APP_SECRET:"fbSecret"
+
+}
